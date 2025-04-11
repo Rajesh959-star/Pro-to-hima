1502702f98adc1d5b811f83361cb292075281bc0
Rajesh959-star <raaaajesh959@gmail.com>
2025-04-11 15:09:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("proposalBtn").addEventListener("click", function () {
+  const msg = document.getElementById("proposalMessage");
+  msg.classList.remove("hidden");
+  msg.style.display = "block";
+});
